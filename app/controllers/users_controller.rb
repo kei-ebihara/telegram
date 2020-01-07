58cef0a4c05d7682a83244840c447d37dc2ba7dc
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2020-01-07 15:02:48 +0900

    ユーザーの詳細ページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.with_attached_image.where(user_id: @user.id)
+                  .order(created_at: :desc)
+                  .page(params[:page]).per(12)
+  end
+end
